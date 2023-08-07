73fd4f62f1b9ff4eb105c0b0a81e6448fae7a3cb
AdelSamy00 <adelsamy188@gmail.com>
2023-08-07 13:24:38 +0300

    ready to deploy

--- src/Home.jsx
+++ src/Home.jsx
@@ -64,7 +64,7 @@ export default function Home() {
         console.log(err);
         navigate('/');
       });
-  }, []);
+  }, [auth, navigate]);
   return (
     <div>
       <div className="mb-32">
