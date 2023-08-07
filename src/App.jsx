ea828f5bff91913c2cef6e0a735f5a35718ac508
AdelSamy00 <adelsamy188@gmail.com>
2023-08-07 13:03:09 +0300

    ready to deploy

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,7 @@ import Register from './Register';
 import Home from './Home';
 
 function App() {
-  axios.defaults.baseURL =
-    'https://todolist-api-4n7m.onrender.com/getUser/64c9043bc63fffca5798ed3c';
+  axios.defaults.baseURL = 'https://todolist-api-4n7m.onrender.com';
   axios.defaults.withCredentials = true;
   return (
     <>
